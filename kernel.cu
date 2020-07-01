1ad15b131de75d7a381a6daa6462fce682be1e0c
Mr-Crocodile <hnxals@gmail.com>
2020-07-01 12:44:29 -0400

    done cuda part

--- kernel.cu
+++ kernel.cu
@@ -45,8 +45,55 @@ void CLoopMain() {
 	PrintResult(p);
 }
 
-void CUDAMain() {
+__global__ void MatrixMutilplicationKernel(int* Md, int* Nd, int* Pd, int width) {
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
 
+	int Pvalue = 0;
+
+	for (int k = 0; k < width; ++k)
+	{
+		int Mdelement = Md[ty * width + k];
+		int Ndelement = Nd[k * width + tx];
+		Pvalue += Mdelement * Ndelement;
+	}
+
+	Pd[ty * width + tx] = Pvalue;
+}
+
+void CUDAMatrixMutilplication(int* m, int* n, int* p, int width) {
+	int size = width * width * sizeof(int);
+	int* Md; 
+	int* Nd; 
+	int* Pd;
+	cudaMalloc((void**)&Md, size);
+	cudaMemcpy(Md, m, size, cudaMemcpyHostToDevice);
+	cudaMalloc((void**)&Nd, size);
+	cudaMemcpy(Nd, n, size, cudaMemcpyHostToDevice);
+	cudaMalloc((void**)&Pd, size);
+
+	dim3 dimBlock(width, width);
+	dim3 dimGrid(1, 1);
+	MatrixMutilplicationKernel <<<dimGrid, dimBlock>>> (Md, Nd, Pd, width);
+
+	cudaMemcpy(p, Pd, size, cudaMemcpyDeviceToHost);
+	cudaFree(Md);
+	cudaFree(Nd);
+	cudaFree(Pd);
+}
+
+void CUDAMain() {
+	int m[256], n[256], p[256];
+	for (int i = 0; i < 256; i++)
+	{
+		m[i] = 1;
+		n[i] = 2;
+		p[i] = 0;
+	}
+
+	CUDAMatrixMutilplication(m, n, p, 16);
+	printf("CUDA result:\n");
+	PrintResult(p);
 }
 
 
